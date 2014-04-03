d698d69eb2890c8b90cea838e60993e1195f8eec
DavidW <djrival7@gmail.com>
2014-04-02 23:40:33 -0400

    Got pictures working

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,15 +11,9 @@ class Project
   field :name, type: String
   field :description, type: String
   
-  has_mongoid_attached_file :photo,
-    :path => ':attachment/:id/:style.:extension',
-    :styles => {
-      :original => ['1920x1680>', :jpg],
-      :small    => ['100x100#',   :jpg],
-      :medium   => ['250x250',    :jpg],
-      :large    => ['500x500>',   :jpg]
-    },
-    :convert_options => { :all => '-background white -flatten +matte' }
-    
+  has_mongoid_attached_file :photo, :styles => { :small => "150x150>" },  
+  :url => "/assets/products/:id/:style/:basename.:extension",  
+  :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension" 
+
 end
 
